56f238f1d7d90a62b85e4e37cd8d361ee5d9e872
Your Name <james@r0bertson.co.uk>
2012-10-15 15:38:52 +0100

    The Sinatra style redirect is now supported

--- lib/rack-rscript.rb
+++ lib/rack-rscript.rb
@@ -8,20 +8,29 @@ require 'app-routes'
 require 'requestor'
 require 'logger'
 
+class Redirect
+  attr_reader :to_url
+  
+  def initialize(url)
+    @to_url = url
+  end
+end
+
 class RackRscript
   include AppRoutes
 
   def initialize(raw_opts={})
     @params = {}
-
+    
     opts = {logfile: '', logrotate: 'daily', pkg_src: ''}.merge(raw_opts)
     @url_base = opts[:pkg_src] # web server serving the RSF files
     @url_base += '/' unless @url_base[-1] == '/'
+    
     @log = false
 
     if opts[:logfile].length > 0 then
       @log = true
-      @logger = Logger.new(opts[:logfile], opts[:logrotate])
+      @logger = Logger.new(opts[:logfile], opts[:logrotate])    
     end
 
     super() # required for app-routes initialize method to exectue
@@ -29,24 +38,36 @@ class RackRscript
   end
 
   def call(env)
+    @env = env
     request = env['REQUEST_URI'][/https?:\/\/[^\/]+(.*)/,1]
 
-    log Time.now.to_s + ": " + request.inspect
-    content, content_type, status_code = run_route(request)
-    if content.nil? then
-      e = $!
-      log(e) if e
-      content, status_code  = "404: page not found", 404 
-    end
+    log "_: " + env.keys.inspect
+    log Time.now.to_s + "_: " + env.inspect
+    content, content_type, status_code = run_route(request)        
 
-    content_type ||= 'text/html'
-    status_code ||= 200
-    [status_code, {"Content-Type" => content_type}, [content]]
+    if content.is_a? Redirect then
+      redirectx = content
+      res = Rack::Response.new
+      res.redirect(redirectx.to_url)
+      res.finish      
+    else
+      
+      if content.nil? then
+        e = $!
+        log(e) if e
+        content, status_code  = "404: page not found", 404             
+      end      
+      
+      content_type ||= 'text/html'
+      status_code ||= 200      
+      
+      [status_code, {"Content-Type" => content_type}, [content]]
+    end
   end
 
 
   def run_job(url, jobs, params={}, *qargs)
-
+    
     if @params[:splat] then
       @params.each do  |k,v|
         @params.delete k unless k == :splat or k == :package or k == :job or k == :captures
@@ -59,8 +80,8 @@ class RackRscript
         v ? r.merge(k[/\w+$/].to_sym => v) : r
       end
       @params.merge! h
-    end
-
+    end    
+    
     result, args = RScript.new.read([url, jobs.split(/\s/), \
       qargs].flatten)
     rws = self
@@ -83,6 +104,10 @@ class RackRscript
     end
   end
   
+  def redirect(url)
+    Redirect.new url
+  end
+  
   private
 
   def default_routes(params)
@@ -95,8 +120,8 @@ class RackRscript
       raw_args = params[:splat]
       args = raw_args.first[1..-1][/.[\/\w]+/].split('/')
       run_job("%s%s.rsf" % [@url_base, package], "//job:" + job, params, args)
-    end        
+    end
+
   end
-  
-   
+     
 end
